352efe15e7f1b8754713f8485555b561de649614
Manon Moutier <manon.moutier@hotmail.fr>
2024-12-03 14:17:33 +0100

    Layout & about creation

---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,46 @@
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <header>
+        <section>
+          <button type="button">
+            <a href="/">
+              <img
+                src="../../public/images/Logo.webp"
+                alt="Logo Reunite & Resolve"
+              />
+            </a>
+          </button>
+        </section>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/dashboard">Personnes disparues</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">À propos</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Outlet />
+
+      <footer>
+        <Link to="/contact">
+          <p>Contact</p>
+        </Link>
+        <p>© by Reunite & Resolve</p>
+        <a href="https://www.francenum.gouv.fr/guides-et-conseils/developpement-commercial/site-web/rediger-des-conditions-generales-dutilisation">
+          CGU & Conditions
+        </a>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
